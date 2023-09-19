ac2245c1155173cfc8d4098f47c539a9a9027b61
Guilherme Ramon Rodrigues da Silva <gramon.silva@hotmail.com>
2023-09-19 01:45:24 -0300

    so far trying to compile

---
+++ dist/CMakeFiles/nrp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nrp.dir/src/main.cpp.obj"
+  "CMakeFiles/nrp.dir/src/main.cpp.obj.d"
+  "libnrp.dll.a"
+  "nrp.exe"
+  "nrp.exe.manifest"
+  "nrp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nrp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dist/CMakeFiles/nrp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for nrp.
+# This may be replaced when dependencies are built.
